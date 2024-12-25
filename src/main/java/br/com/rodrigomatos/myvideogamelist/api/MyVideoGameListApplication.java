9eabe25c556335f03edf447e9f42000d8c0c1988
Rodrigo Cândido Matos <rodriguou2k20@gmail.com>
2024-12-25 13:57:42 -0300

    feat: initial commit

---
+++ src/main/java/br/com/rodrigomatos/myvideogamelist/api/MyVideoGameListApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigomatos.myvideogamelist.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVideoGameListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVideoGameListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodrigomatos/myvideogamelist/api/MyVideoGameListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigomatos.myvideogamelist.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyVideoGameListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
