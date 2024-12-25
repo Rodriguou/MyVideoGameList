1d2cf0f220355f3b5fc7221b9cbf295b01a4a884
Rodrigo Cândido Matos <rodriguou2k20@gmail.com>
2024-12-25 19:16:11 -0300

    feat: add GameRepository interface for Game entity

---
+++ src/main/java/br/com/rodrigomatos/myvideogamelist/api/repository/GameRepository.java
@@ -0,0 +1,16 @@
+package br.com.rodrigomatos.myvideogamelist.api.repository;
+
+import br.com.rodrigomatos.myvideogamelist.api.entity.Game;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+    List<Game> findByNameContainingIgnoreCase(String name, Sort sort);
+
+    @Query("SELECT g FROM Game g WHERE YEAR(g.releaseDate) = :year")
+    List<Game> findByReleaseYear(@Param("year") int year, Sort sort);
+}
