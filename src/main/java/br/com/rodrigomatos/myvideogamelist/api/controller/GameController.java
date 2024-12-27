544541363bfb968ef6016f39db4588fdaeae9d38
Rodrigo Cândido Matos <rodriguou2k20@gmail.com>
2024-12-27 17:03:49 -0300

    refactor: update GameController to use ResponseEntity and improve endpoint paths

--- src/main/java/br/com/rodrigomatos/myvideogamelist/api/controller/GameController.java
+++ src/main/java/br/com/rodrigomatos/myvideogamelist/api/controller/GameController.java
@@ -3,6 +3,8 @@ package br.com.rodrigomatos.myvideogamelist.api.controller;
 import br.com.rodrigomatos.myvideogamelist.api.entity.Game;
 import br.com.rodrigomatos.myvideogamelist.api.service.GameService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,37 +16,44 @@ public class GameController {
     private final GameService gameService;
 
     @PostMapping
-    public Game addGame(@RequestBody Game game) {
-        return gameService.addGame(game);
+    public ResponseEntity<Game> addGame(@RequestBody Game game) {
+        Game createdGame = gameService.addGame(game);
+        return new ResponseEntity<>(createdGame, HttpStatus.CREATED);
     }
 
     @GetMapping("/name")
-    public List<Game> getAllGamesSortedByName() {
-        return gameService.getAllGamesSortedByName();
+    public ResponseEntity<List<Game>> getAllGamesSortedByName() {
+        List<Game> games = gameService.getAllGamesSortedByName();
+        return new ResponseEntity<>(games, HttpStatus.OK);
     }
 
     @GetMapping("/release-date")
-    public List<Game> getAllGamesSortedByReleaseDate() {
-        return gameService.getAllGamesSortedByReleaseDate();
+    public ResponseEntity<List<Game>> getAllGamesSortedByReleaseDate() {
+        List<Game> games = gameService.getAllGamesSortedByReleaseDate();
+        return new ResponseEntity<>(games, HttpStatus.OK);
     }
 
-    @GetMapping("/name/{name}")
-    public List<Game> getGamesByName(String name) {
-        return gameService.getGamesByName(name);
+    @GetMapping("/search/by-name")
+    public ResponseEntity<List<Game>> getGamesByName(@RequestParam String name) {
+        List<Game> games = gameService.getGamesByName(name);
+        return new ResponseEntity<>(games, HttpStatus.OK);
     }
 
-    @GetMapping("/release-year/{year}")
-    public List<Game> getGamesByReleaseYear(int year) {
-        return gameService.getGamesByReleaseYear(year);
+    @GetMapping("/search/by-release-year")
+    public ResponseEntity<List<Game>> getGamesByReleaseYear(@RequestParam Integer year) {
+        List<Game> games = gameService.getGamesByReleaseYear(year);
+        return new ResponseEntity<>(games, HttpStatus.OK);
     }
 
-    @PutMapping
-    public Game editGame(Long id, Game updatedGame) {
-        return gameService.editGame(id, updatedGame);
+    @PutMapping("/{id}")
+    public ResponseEntity<Game> editGame(@PathVariable("id") Long id, @RequestBody Game updatedGame) {
+        Game editedGame = gameService.editGame(id, updatedGame);
+        return new ResponseEntity<>(editedGame, HttpStatus.OK);
     }
 
-    @DeleteMapping("{id}")
-    public void deleteGame(@PathVariable("id") Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteGame(@PathVariable("id") Long id) {
         gameService.deleteGame(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
