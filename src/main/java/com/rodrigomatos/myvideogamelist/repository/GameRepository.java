1b335f2afc010f9ef3a150bf85299d7caa42c330
Rodrigo Cândido Matos <rodriguou2k20@gmail.com>
2025-01-01 18:00:20 -0300

    refactor: adjust GameRepository by removing annotation and modifying parameter type

--- src/main/java/com/rodrigomatos/myvideogamelist/repository/GameRepository.java
+++ src/main/java/com/rodrigomatos/myvideogamelist/repository/GameRepository.java
@@ -1,16 +1,17 @@
-package br.com.rodrigomatos.myvideogamelist.api.repository;
+package com.rodrigomatos.myvideogamelist.repository;
 
-import br.com.rodrigomatos.myvideogamelist.api.entity.Game;
+import com.rodrigomatos.myvideogamelist.entity.Game;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
+
     List<Game> findByNameContainingIgnoreCase(String name, Sort sort);
 
     @Query("SELECT g FROM Game g WHERE YEAR(g.releaseDate) = :year")
-    List<Game> findByReleaseYear(@Param("year") Integer year, Sort sort);
+    List<Game> findByReleaseYear(int year, Sort sort);
+
 }
