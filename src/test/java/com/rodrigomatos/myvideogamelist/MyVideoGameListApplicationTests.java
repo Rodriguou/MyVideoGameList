cad409610d1528f6462bede402754443f2cb673a
Rodrigo Cândido Matos <rodriguou2k20@gmail.com>
2025-01-04 15:34:53 -0300

    refactor: update package name in MyVideoGameListApplicationTests

--- src/test/java/com/rodrigomatos/myvideogamelist/MyVideoGameListApplicationTests.java
+++ src/test/java/com/rodrigomatos/myvideogamelist/MyVideoGameListApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.rodrigomatos.myvideogamelist.api;
+package com.rodrigomatos.myvideogamelist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
