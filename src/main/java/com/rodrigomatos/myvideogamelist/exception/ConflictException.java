a62573e9c753242af1554dcfc4374331b48ecf19
Rodrigo Cândido Matos <rodriguou2k20@gmail.com>
2025-01-09 20:27:10 -0300

    feat: add custom ConflictException class

---
+++ src/main/java/com/rodrigomatos/myvideogamelist/exception/ConflictException.java
@@ -0,0 +1,9 @@
+package com.rodrigomatos.myvideogamelist.exception;
+
+public class ConflictException extends RuntimeException {
+
+    public ConflictException(String message) {
+        super(message);
+    }
+
+}
