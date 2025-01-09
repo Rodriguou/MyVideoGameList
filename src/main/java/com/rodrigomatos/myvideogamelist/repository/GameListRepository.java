c67fb7225228e68480ac41b6f4ac793e1fe4db40
Rodrigo Cândido Matos <rodriguou2k20@gmail.com>
2025-01-09 20:09:21 -0300

    feat: add additional query methods to GameListRepository

--- src/main/java/com/rodrigomatos/myvideogamelist/repository/GameListRepository.java
+++ src/main/java/com/rodrigomatos/myvideogamelist/repository/GameListRepository.java
@@ -1,10 +1,24 @@
 package com.rodrigomatos.myvideogamelist.repository;
 
 import com.rodrigomatos.myvideogamelist.entity.GameList;
+import com.rodrigomatos.myvideogamelist.entity.GameStatus;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
 
     boolean existsByGameId(Long gameId);
+
+    @Modifying
+    @Query("DELETE FROM GameList gl WHERE gl.game.id = :gameId")
+    void deleteByGameId(Long gameId);
+
+    List<GameList> findByGameNameContainingIgnoreCase(String name, Sort sort);
+
+    List<GameList> findByStatus(GameStatus status);
 
 }
