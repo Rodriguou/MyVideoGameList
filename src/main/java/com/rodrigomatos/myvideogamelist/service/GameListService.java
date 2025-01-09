ce8db9941219287171ad05bd2e5f59c9755a06e1
Rodrigo Cândido Matos <rodriguou2k20@gmail.com>
2025-01-09 20:17:09 -0300

    feat: replace DataIntegrityViolationException with custom ConflictException and add new methods for managing GameList

--- src/main/java/com/rodrigomatos/myvideogamelist/service/GameListService.java
+++ src/main/java/com/rodrigomatos/myvideogamelist/service/GameListService.java
@@ -4,11 +4,14 @@ import com.rodrigomatos.myvideogamelist.dto.GameListDTO;
 import com.rodrigomatos.myvideogamelist.entity.Game;
 import com.rodrigomatos.myvideogamelist.entity.GameList;
 import com.rodrigomatos.myvideogamelist.entity.GameStatus;
+import com.rodrigomatos.myvideogamelist.exception.ConflictException;
 import com.rodrigomatos.myvideogamelist.repository.GameListRepository;
 import com.rodrigomatos.myvideogamelist.repository.GameRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +21,7 @@ public class GameListService {
 
     public GameListDTO addGameToList(GameListDTO gameListDTO) {
         if (gameListRepository.existsByGameId(gameListDTO.gameId())) {
-            throw new DataIntegrityViolationException("Game already in list.");
+            throw new ConflictException("This game is already in your list");
         }
 
         Game game = gameRepository.findById(gameListDTO.gameId()).orElseThrow();
@@ -27,6 +30,63 @@ public class GameListService {
         gameList = gameListRepository.save(gameList);
 
         return new GameListDTO(gameList.getId(), game.getId(), game.getName(), game.getReleaseDate(), gameList.getStatus());
+    }
+
+    public List<GameListDTO> getGameListSortedByName() {
+        return convertToDTOList(
+                gameListRepository.findAll(Sort.by(
+                        Sort.Order.asc("game.name"),
+                        Sort.Order.asc("game.releaseDate")
+                ))
+        );
+    }
+
+    public List<GameListDTO> getGameListSortedByReleaseDate() {
+        return convertToDTOList(
+                gameListRepository.findAll(Sort.by(
+                        Sort.Order.asc("game.releaseDate"),
+                        Sort.Order.asc("game.name")
+                ))
+        );
+    }
+
+    public List<GameListDTO> findGamesByName(String name) {
+        return convertToDTOList(
+                gameListRepository.findByGameNameContainingIgnoreCase(
+                        name,
+                        Sort.by(Sort.Order.asc("game.name"), Sort.Order.asc("game.releaseDate"))
+                )
+        );
+    }
+
+    public List<GameListDTO> getGameListByStatus(GameStatus status) {
+        return convertToDTOList(
+                gameListRepository.findByStatus(status)
+        );
+    }
+
+    public GameListDTO getGameById(Long id) {
+        GameList gameList = gameListRepository.findById(id).orElseThrow();
+        return new GameListDTO(gameList.getId(), gameList.getGame().getId(), gameList.getGame().getName(), gameList.getGame().getReleaseDate(), gameList.getStatus());
+    }
+
+    public GameListDTO updateGameStatus(Long id, GameStatus status) {
+        GameList gameList = gameListRepository.findById(id).orElseThrow();
+        gameList.setStatus(status);
+        gameList = gameListRepository.save(gameList);
+
+        return new GameListDTO(gameList.getId(), gameList.getGame().getId(), gameList.getGame().getName(), gameList.getGame().getReleaseDate(), gameList.getStatus());
+    }
+
+    public void removeGameFromList(Long id) {
+        GameList gameList = gameListRepository.findById(id).orElseThrow();
+        gameListRepository.deleteById(id);
+    }
+
+    private List<GameListDTO> convertToDTOList(List<GameList> gameList) {
+        return gameList.stream()
+                .map(game -> new GameListDTO(game.getId(), game.getGame().getId(), game.getGame().getName(), game.getGame().getReleaseDate(), game.getStatus()))
+                .toList();
     }
 
 }
