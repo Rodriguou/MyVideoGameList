cf8c89096023810c52bbf9309bd33221cf690b6d
Rodrigo Cândido Matos <rodriguou2k20@gmail.com>
2025-01-09 20:33:04 -0300

    feat: add endpoints for managing the game list

--- src/main/java/com/rodrigomatos/myvideogamelist/controller/GameListController.java
+++ src/main/java/com/rodrigomatos/myvideogamelist/controller/GameListController.java
@@ -1,15 +1,15 @@
 package com.rodrigomatos.myvideogamelist.controller;
 
 import com.rodrigomatos.myvideogamelist.dto.GameListDTO;
+import com.rodrigomatos.myvideogamelist.entity.GameStatus;
 import com.rodrigomatos.myvideogamelist.service.GameListService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/game-list")
@@ -22,4 +22,47 @@ public class GameListController {
         GameListDTO createdGameList = gameListService.addGameToList(gameListDTO);
         return new ResponseEntity<>(createdGameList, HttpStatus.CREATED);
     }
+
+    @GetMapping("sorted-by-name")
+    public ResponseEntity<List<GameListDTO>> getGameListSortedByName() {
+        List<GameListDTO> gameList = gameListService.getGameListSortedByName();
+        return new ResponseEntity<>(gameList, HttpStatus.OK);
+    }
+
+    @GetMapping("sorted-by-release-date")
+    public ResponseEntity<List<GameListDTO>> getGameListByReleaseDate() {
+        List<GameListDTO> gameList = gameListService.getGameListSortedByReleaseDate();
+        return new ResponseEntity<>(gameList, HttpStatus.OK);
+    }
+
+    @GetMapping("search")
+    public ResponseEntity<List<GameListDTO>> findGamesByName(@RequestParam String name) {
+        List<GameListDTO> gameList = gameListService.findGamesByName(name);
+        return new ResponseEntity<>(gameList, HttpStatus.OK);
+    }
+
+    @GetMapping("status/{status}")
+    public ResponseEntity<List<GameListDTO>> getGameListByStatus(@PathVariable("status") String status) {
+        List<GameListDTO> gameList = gameListService.getGameListByStatus(GameStatus.valueOf(status.toUpperCase()));
+        return new ResponseEntity<>(gameList, HttpStatus.OK);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<GameListDTO> getGameById(@PathVariable("id") Long id) {
+        GameListDTO gameList = gameListService.getGameById(id);
+        return new ResponseEntity<>(gameList, HttpStatus.OK);
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<GameListDTO> updateGameStatus(@PathVariable("id") Long id, @RequestParam String status) {
+        GameListDTO updatedGameList = gameListService.updateGameStatus(id, GameStatus.valueOf(status.toUpperCase()));
+        return new ResponseEntity<>(updatedGameList, HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> removeGameFromList(@PathVariable("id") Long id) {
+        gameListService.removeGameFromList(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
