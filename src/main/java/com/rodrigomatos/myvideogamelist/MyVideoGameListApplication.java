e1b82c8400e0b3ba88ce88f0c65e5c559ba996bf
Rodrigo Cândido Matos <rodriguou2k20@gmail.com>
2025-02-23 18:24:06 -0300

    style: reformat code in MyVideoGameListApplication

--- src/main/java/com/rodrigomatos/myvideogamelist/MyVideoGameListApplication.java
+++ src/main/java/com/rodrigomatos/myvideogamelist/MyVideoGameListApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyVideoGameListApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyVideoGameListApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyVideoGameListApplication.class, args);
+    }
 
 }
